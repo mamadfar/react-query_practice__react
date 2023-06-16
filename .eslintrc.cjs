2e58c41181086839b98aec796a8352df0e8dabce
mohammad farhadi <m.sabah.farhadi@gmail.com>
2023-06-16 23:54:54 +0200

    initial commit && add react-query and react-query-devtools

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    "plugin:@tanstack/eslint-plugin-query/recommended",
+    "react-app"
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react-refresh', "@tanstack/query"],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+  },
+}
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+        <div>Mamad</div>
+    </main>
+  )
+}
+
+export default App
